03a6d7199e304b3fedc3a9070491c6d7bad92577
Apprentice <jmoon018@ucr.edu>
2015-03-05 18:44:49 -0800

    created user controller, css files for profile (used for both reps and users)... created basic html layout that draws info from db too. TODO: RESPONSIVE SCSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :pledges
+
+  def fulfilled_pledge_cnt
+    self.pledges.where(fulfilled: true).count
+  end
+
+  def pledge_cnt
+    self.pledges.count
+  end
 end
