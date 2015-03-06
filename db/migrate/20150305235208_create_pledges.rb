f473c24242c69afbf1a5b13b0ce0d89a59dde2d2
Apprentice <jmoon018@ucr.edu>
2015-03-06 15:12:13 -0800

    adds columns for thumbnails and twitter handles, both user and rep for each

--- db/migrate/20150305235208_create_pledges.rb
+++ db/migrate/20150305235208_create_pledges.rb
@@ -8,6 +8,7 @@ class CreatePledges < ActiveRecord::Migration
       t.boolean :fulfilled, default: :false
       t.boolean :positive
       t.string :user_thumbnail_url
+      t.string :rep_thumbnail_url
       t.string :user_twitter_handle
       t.string :rep_twitter_handle
 
