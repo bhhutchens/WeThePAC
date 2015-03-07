6d89adecb653a4641cc780b912605d61d2710fa0
Apprentice <jmoon018@ucr.edu>
2015-03-06 20:42:44 -0800

    adds background flag and some javascript?

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,54 +1,64 @@
-// methods
+// input: jquery selector
+// output: handlebar template to be appended
+function compileTemplate (selector) {
+  return Handlebars.compile($(selector).html());
+}
 
-
-
-
-
-$(document).ready(function() {
-  // get a user
-  console.log("users js loaded");
-
-  $.ajax({
-    url: api_server + "users/1",
-    type: "GET"
-  }).done(function(data) {
-    console.log(data);
-    var user_template = $("#user_info").html();
-    var compiled_user_template = Handlebars.compile(user_template);
-
-    // usage: apnd (compiled_template ({ key: data }))
-    var apnd = function(data) { $(".profile_upper").append(data); }
-
-    //apnd(compiled_user_template({user: data}));
-    apnd(compiled_user_template({user:data}))
-
-    //get_pledges_feed();
-  }).fail(function(data){
-    console.log("failed getting a user with ajax call");
-  })
-
-  // get pledges feed
-  //get_pledges_feed();
-});
-
-// ajax call for pledge feed
-function get_pledges_feed() {
+// gets pledges thru ajax
+// renders them onto the page as a list
+function renderPledges() {
+  // ajax GET call
   $.ajax({
     url: api_server + "users/1/pledges",
-    type: "GET"
-  }).done(function(data){
-    console.log("succes getting user's pledges");
-  // title the pledge feed. Remove this after adding #pledges div.p
-  $('#tweet_list').prepend("<h1>Pledges</h1>")
-    // iterate through each pledge response and append it to the page
+  }).
+  done(function(data) {
+    // add the name of the list in html
+    console.log("success getting user's pledges");
+    $('#tweet_list').prepend("<h1>Pledges</h1>");
+
+    // loop through each pledge and append it as a list item
     $.each(data, function(index, pledge) {
-      var user_pledge_feed_template = $("#user_pledge_feed").html();
-      var compiled_pledge_feed_template = Handlebars.compile(user_pledge_feed_template);
-      console.log(pledge);
-      $("#tweet_list").append(compiled_pledge_feed_template({pledge: pledge}));
+
+      // get and compile templates
+      var template = compileTemplate("#user_pledge_feed");
+
+      // append to list -- var name: pledge
+      $("#tweet_list").append(template({pledge: pledge}));
     });
-  }).fail(function(){
+  }).
+  fail(function(){
     console.log('unable to get pledges feed');
   });
 }
 
+
+// gets user information (name, handle, img url, etc) thru ajax
+// renders it onto the page, then renders the pledge feed
+function getUserInfo() {
+  $.ajax({
+    url: api_server + "users/1",
+    type: "GET"
+  }).
+  done(function(data) {
+    console.log("Successfully received user data");
+
+    // get + append template
+    var userInfoTemplate = compileTemplate("#user_info");
+    $(".profile_upper").append(userInfoTemplate({user: data}));
+
+    renderPledges(); // RENDER THE PLEDGES FEED
+  }).
+  fail(function(data) {
+    console.log("failed getting a user with an ajax call");
+  })
+}
+
+// DOCUMENT READY
+// TODO: ONLY WORKS FOR USER 1
+//============================
+
+$(document).ready(function() {
+  // populate the page w/ data
+  console.log("users js loaded");
+  getUserInfo();
+});
