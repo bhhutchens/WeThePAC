fb17482e28c726eab5f32abd4f49b5e98ea39b2e
Apprentice <jmoon018@ucr.edu>
2015-03-06 17:26:59 -0800

    all the data displays on the user profile page ... it has basic styling and responsiveness. todo: refactor code

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,9 @@
+// methods
+
+
+
+
+
 $(document).ready(function() {
   // get a user
   console.log("users js loaded");
@@ -11,30 +17,38 @@ $(document).ready(function() {
     var compiled_user_template = Handlebars.compile(user_template);
 
     // usage: apnd (compiled_template ({ key: data }))
-    var apnd = function(data) { $("body").append(data); }
+    var apnd = function(data) { $(".profile_upper").append(data); }
 
     //apnd(compiled_user_template({user: data}));
     apnd(compiled_user_template({user:data}))
+
+    //get_pledges_feed();
   }).fail(function(data){
     console.log("failed getting a user with ajax call");
   })
 
-// get pledges feed
-$.ajax({
-  url: api_server + "users/1/pledges",
-  type: "GET"
-}).done(function(data){
-  console.log("succes getting user's pledges");
+  // get pledges feed
+  //get_pledges_feed();
+});
+
+// ajax call for pledge feed
+function get_pledges_feed() {
+  $.ajax({
+    url: api_server + "users/1/pledges",
+    type: "GET"
+  }).done(function(data){
+    console.log("succes getting user's pledges");
   // title the pledge feed. Remove this after adding #pledges div.p
-  $('body').append("<h1>Pledges</h1>")
+  $('#tweet_list').prepend("<h1>Pledges</h1>")
     // iterate through each pledge response and append it to the page
     $.each(data, function(index, pledge) {
       var user_pledge_feed_template = $("#user_pledge_feed").html();
       var compiled_pledge_feed_template = Handlebars.compile(user_pledge_feed_template);
       console.log(pledge);
-      $('body').append(compiled_pledge_feed_template({pledge: pledge}));
+      $("#tweet_list").append(compiled_pledge_feed_template({pledge: pledge}));
     });
   }).fail(function(){
     console.log('unable to get pledges feed');
   });
-});
+}
+
