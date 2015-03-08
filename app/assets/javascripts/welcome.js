e396d1ba6cad2aca41b95b67cce2f2efb511a88e
adellhk <adell.hk@gmail.com>
2015-03-08 15:54:37 -0700

    adds beforeSend xhr headers

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -21,56 +21,55 @@ $(document).ready(function(){
       url: "/welcome/search",
       type: "GET",
       dataType: 'JSON',
-      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))}
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+      data: {"searchBarInput": searchTerms}
+    }).done(function(serverData){
+      console.log("db search query success!")
+      console.log("got: "+serverData)
+      removeExistingSearchResults();
+      appendSearchResults(serverData);
+    }).error(function(){
+      console.log('failed')
     })
-    data: {"searchBarInput": searchTerms}
-  }).done(function(serverData){
-    console.log("db search query success!")
-    console.log("got: "+serverData)
-    removeExistingSearchResults();
-    appendSearchResults(serverData);
-  }).error(function(){
-    console.log('failed')
-  })
-});
-
-function getActivityFeed() {
-  $.ajax({
-    url: api_server + "/activity_feed",
-    beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))}
-  }).done(function(data){
-    console.log('success getAndPopulateActivityFeed :)');
-    populateActivityFeed(data);
-  }).fail(function(){
-    console.log('failed to getAndPopulateActivityFeed :(');
   });
-};
 
-getActivityFeed();
-
-function populateActivityFeed(recentPledges){
-  var activityFeedTemplate = compileTemplate('#activityFeedTemplate');
-  $.each(recentPledges, function(index, pledge) {
-    $('#activityFeed').append(activityFeedTemplate({activityFeedItem : pledge}));
-  });
-};
-
-function removeExistingSearchResults(){
-  if ( $('.search-result').length > 0 ) {
-    $('.search-result').remove();
-  };
-};
-
-function appendSearchResults(searchResults){
-  console.log("appending search results");
-  var compiledSearchResultTemplate = Handlebars.compile($("#searchResultTemplate").html());
-  $.each(searchResults, function(index, rep) {
-    if (rep.thumbnail_url === null) {
-      rep.thumbnail_url = server+"/images/no-avatar.jpg" }
-      rep.profile_url = server+"/reps/"+rep.id
-      if (rep.twitter_handle != null) {rep.twitter_display = "@" + rep.twitter_handle} else { rep.twitter_display = ""}
-        $("#searchResults").append(compiledSearchResultTemplate({rep: rep}));
+  function getActivityFeed() {
+    $.ajax({
+      url: api_server + "/activity_feed",
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))}
+    }).done(function(data){
+      console.log('success getAndPopulateActivityFeed :)');
+      populateActivityFeed(data);
+    }).fail(function(){
+      console.log('failed to getAndPopulateActivityFeed :(');
     });
-};
+  };
+
+  getActivityFeed();
+
+  function populateActivityFeed(recentPledges){
+    var activityFeedTemplate = compileTemplate('#activityFeedTemplate');
+    $.each(recentPledges, function(index, pledge) {
+      $('#activityFeed').append(activityFeedTemplate({activityFeedItem : pledge}));
+    });
+  };
+
+  function removeExistingSearchResults(){
+    if ( $('.search-result').length > 0 ) {
+      $('.search-result').remove();
+    };
+  };
+
+  function appendSearchResults(searchResults){
+    console.log("appending search results");
+    var compiledSearchResultTemplate = Handlebars.compile($("#searchResultTemplate").html());
+    $.each(searchResults, function(index, rep) {
+      if (rep.thumbnail_url === null) {
+        rep.thumbnail_url = server+"/images/no-avatar.jpg" }
+        rep.profile_url = server+"/reps/"+rep.id
+        if (rep.twitter_handle != null) {rep.twitter_display = "@" + rep.twitter_handle} else { rep.twitter_display = ""}
+          $("#searchResults").append(compiledSearchResultTemplate({rep: rep}));
+      });
+  };
 
 });
