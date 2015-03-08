0853e749b4654416f03b530f88f7b6058fa61c84
Brandon Hutchens <adell.hk@gmail.com>
2015-03-08 12:25:52 -0700

    added ability to search a zipcode

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,11 +14,31 @@ class WelcomeController < ApplicationController
       keywords -= zips
     end
     puts zips
-    # TODO: call sunlight API and pass "zips". Add results to matches.
 
-    # checks database for reps whose names mach keywords
+    # calls sunlight API and pass "zips". Add results to matches.
+    if zips && zips[0].length == 7
+      returned_reps = get_rep_zips(zips[0][1..5])
+      keywords << returned_reps
+      keywords.flatten!
+    end
+
+    # checks database for reps whose names match keywords
     matches = Rep.where('name ilike any ( array[?] )', keywords).limit(21)
 
     render json: matches, status: 200
   end
+end
+
+
+def get_rep_zips(zip)
+  # calls sunlight api, gets reps from zip, and returns rep names to search
+  response = HTTParty.get("http://congress.api.sunlightfoundation.com/legislators/locate?zip=#{zip}&apikey=db117ccbb61e4b82abc74d37a9b58ed2")
+  rep_names = []
+  response['results'].each do |rep|
+    rep_first_name = '%' + rep['first_name'] + '%'
+    rep_last_name = '%' + rep['last_name'] + '%'
+    rep_names << rep_first_name
+    rep_names << rep_last_name
+  end
+  return rep_names
 end
