04f1740d61b75abaf72e705f8427987c0c8584f1
adellhk <adell.hk@gmail.com>
2015-03-08 15:57:51 -0700

    updates preflight check action on unmatched route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  # http://stackoverflow.com/questions/14734243/rails-csrf-protection-angular-js-protect-from-forgery-makes-me-to-log-out-on
+  after_filter :cors_set_access_control_headers#, :set_csrf_cookie_for_ng
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-# http://stackoverflow.com/questions/14734243/rails-csrf-protection-angular-js-protect-from-forgery-makes-me-to-log-out-on
-  after_filter :cors_set_access_control_headers#, :set_csrf_cookie_for_ng
 
   def cors_set_access_control_headers
     puts "Calling set access control headers"
