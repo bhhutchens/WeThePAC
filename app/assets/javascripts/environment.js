692acbefd16debe8c3718e44395b52357814281f
adellhk <adell.hk@gmail.com>
2015-03-08 15:00:49 -0700

    updates environment.js again

--- app/assets/javascripts/environment.js
+++ app/assets/javascripts/environment.js
@@ -1,2 +1,2 @@
-var api_server = "http://we-the-pac.herokuapp.com/api";
-var server = "http://we-the-pac.herokuapp.com/";
+var api_server = "http://wethepac.net/api";
+var server = "http://wethepac.net";
