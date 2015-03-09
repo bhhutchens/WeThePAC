2ad312f1173037453c9f7c75eb3ecf63e811611e
Yukio Rattai <yukior@gmail.com>
2015-03-08 21:23:54 -0700

    fixed tweet message not saving bug

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -4,14 +4,13 @@ function makeTweet(newMessage) {
     beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
     data: {tweet: {message: newMessage}},
     success: function(response) {
-      postPledge(response.tweet_id);
+      postPledge(response.tweet_id, newMessage);
     }
   });
 }
 
-function postPledge(tweetId) {
+function postPledge(tweetId, newMessage) {
   var positive = $('#tweet-box').data('positive')
-  var tweet_message = $('#tweet-box').val()
 
   // regexp to get any number of digits (#) starting from end ($) of path name
   var repId = location.pathname.match(/\d*$/)[0]
@@ -20,7 +19,7 @@ function postPledge(tweetId) {
     type: 'POST',
     dataType: 'json',
     beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-    data: {tweet_id: tweetId, rep_id: repId, positive: positive, tweet_message: tweet_message}
+    data: {tweet_id: tweetId, rep_id: repId, positive: positive, tweet_message: newMessage}
   })
   .done(function() {
     console.log("successfully posted pledge");
