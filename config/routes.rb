1e26a8089b1cfeaf8b818c6065f094885ca8924c
Jamal Moon <jmoon018@ucr.edu>
2015-03-09 22:16:46 -0700

    added article routes

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,8 @@ Rails.application.routes.draw do
   end
   resources :tweets, only: [:new, :create]
 
+  resources :articles
+
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
