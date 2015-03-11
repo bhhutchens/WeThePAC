926e7c46957cf4397e8f36831b88eb69be160ae5
Brandon Hutchens <adell.hk@gmail.com>
2015-03-10 19:13:29 -0700

    moved search bar to header. works on index and rep page

---
+++ app/assets/javascripts/header-search-bar.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  readySearch();
+})
+
+// attaches event listener to search bar, passes search parameters to index
+function readySearch() {
+  $(".search-form").eq(0).keyup(function(e){
+    e.preventDefault();
+    var searchTerms = $('#searchBarInput').val();
+    console.log("searching /welcome/search with terms: "+searchTerms);
+    $.ajax({
+      url: "/welcome/search",
+      type: "GET",
+      dataType: 'JSON',
+      data: {"searchBarInput": searchTerms}
+    }).done(function(serverData){
+      console.log("db search query success!")
+      console.log("got: "+serverData)
+      removeExistingSearchResults();
+      appendSearchResults(serverData);
+    }).error(function(){
+      console.log('failed')
+    })
+  });
+}
+
+function removeExistingSearchResults(){
+  if ( $('.search-result').length > 0 ) {
+    $('.search-result').remove();
+  };
+};
+
+function appendSearchResults(searchResults){
+  console.log("appending search results");
+  var compiledSearchResultTemplate = Handlebars.compile($("#searchResultTemplate").html());
+  $.each(searchResults, function(index, rep) {
+    if (rep.thumbnail_url === null) {
+      rep.thumbnail_url = "/images/no-avatar.jpg" }
+      rep.profile_url = "/reps/"+rep.id
+      if (rep.twitter_handle != null) {rep.twitter_display = "@" + rep.twitter_handle} else { rep.twitter_display = ""}
+        $("#searchResults").append(compiledSearchResultTemplate({rep: rep}));
+    });
+};
+
+function compileTemplate (selector) {
+  return Handlebars.compile($(selector).html());
+}
