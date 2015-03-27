ce877304fd088e83403ebc40620f93495f257869
Ukeeyo <yukior@gmail.com>
2015-03-27 13:27:03 -0700

    added schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310175305) do
+ActiveRecord::Schema.define(version: 20150327194359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150310175305) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "bitly"
   end
 
   create_table "articles_reps", id: false, force: :cascade do |t|
